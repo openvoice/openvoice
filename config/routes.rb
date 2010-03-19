00e5f215dcab466379ba2592a0052b09a8672cf3
Zhao Lu <zlu@zlu-imac.local>
2010-03-18 21:05:52 -0700

    added logout link

--- config/routes.rb
+++ config/routes.rb
@@ -33,10 +33,11 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
   map.connect 'phone_numbers/get_user', :controller => 'phone_numbers', :action => 'locate_user'
+  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
 
   # See how all your routes lay out with "rake routes"
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  map.root :controller => "user_sessions", :action => "new"
   map.resource :account, :controller => "users"
   map.resources :users do |user|
     user.resources :phone_numbers
