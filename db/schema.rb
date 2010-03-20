0200c3a4375afa400b8de9230a2bf4fe640d0144
Zhao Lu <zlu@me.com>
2010-03-19 17:32:15 -0700

    change user_id type from string to integer for contacts

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100319230821) do
+ActiveRecord::Schema.define(:version => 20100320003133) do
 
   create_table "call_logs", :force => true do |t|
     t.string   "from"
