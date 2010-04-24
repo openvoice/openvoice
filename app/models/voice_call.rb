29529dfcae2a8296e923b152f690dca41975265d
Zhao Lu <zlu@me.com>
2010-04-23 17:11:29 -0700

    call on the number field of phone_number model

--- app/models/voice_call.rb
+++ app/models/voice_call.rb
@@ -5,7 +5,7 @@ class VoiceCall < ActiveRecord::Base
   after_create :dial
 
   def dial
-    call_url = 'http://api.tropo.com/1.0/sessions?action=create&token=' + OUTBOUND_VOICE_TEMP + '&to=' + to + '&from=' + user.phone_numbers.first
+    call_url = 'http://api.tropo.com/1.0/sessions?action=create&token=' + OUTBOUND_VOICE_TEMP + '&to=' + to + '&from=' + user.phone_numbers.first.number
                 # TODO probably change into a primary number, mostly likely a pstn number
 
 
