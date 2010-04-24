36e19622510db6d0512bbae0b7bd5bae46dace23
Zhao Lu <zlu@me.com>
2010-04-23 17:03:57 -0700

    added from field for outbound call

--- app/models/voice_call.rb
+++ app/models/voice_call.rb
@@ -5,7 +5,12 @@ class VoiceCall < ActiveRecord::Base
   after_create :dial
 
   def dial
-    call_url = 'http://api.tropo.com/1.0/sessions?action=create&token=' + OUTBOUND_VOICE_TEMP + '&to=' + to
+    call_url = 'http://api.tropo.com/1.0/sessions?action=create&token='
+                + OUTBOUND_VOICE_TEMP
+                + '&to=' + to
+                # TODO probably change into a primary number, mostly likely a pstn number
+                + '&from=' + user.phone_numbers.first
+
     open(call_url) do |r|
       p r
     end
