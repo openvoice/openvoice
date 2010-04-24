4ca683d7a611934118671eb1082ac26eb47ab5c9
Zhao Lu <zlu@me.com>
2010-04-23 17:06:06 -0700

    adding line connector

--- app/models/voice_call.rb
+++ app/models/voice_call.rb
@@ -5,9 +5,9 @@ class VoiceCall < ActiveRecord::Base
   after_create :dial
 
   def dial
-    call_url = 'http://api.tropo.com/1.0/sessions?action=create&token='
-                + OUTBOUND_VOICE_TEMP
-                + '&to=' + to
+    call_url = 'http://api.tropo.com/1.0/sessions?action=create&token=' \
+                + OUTBOUND_VOICE_TEMP \
+                + '&to=' + to \
                 # TODO probably change into a primary number, mostly likely a pstn number
                 + '&from=' + user.phone_numbers.first
 
