2992941a432e32e5dccdf1fb459cff4551be3842
Zhao Lu <zlu@zlu-imac.local>
2010-05-03 20:51:28 -0700

    user can send outbound sms without tropo script

--- app/models/messaging.rb
+++ app/models/messaging.rb
@@ -3,10 +3,15 @@ class Messaging < ActiveRecord::Base
 
   def send_text
     if outgoing
+      msg_url = 'http://api.tropo.com/1.0/sessions?action=create&token=' + OUTBOUND_TOEKN_MESSAGING + '&from='+ from + '&to=' + to + '&text=' + CGI::escape(text)
+      open(msg_url)
+    end
+  end
+
+  def send_text1
+    if outgoing
       msg_url = 'http://api.tropo.com/1.0/sessions?action=create&token=' + OUTBOUND_MESSAGING_TEMP + '&from='+ from + '&to=' + to + '&text=' + CGI::escape(text)
-      open(msg_url) do |r|
-        p r
-      end
+      open(msg_url)
     end
   end
 end
