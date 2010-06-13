2cc987626f5552f174e8062c3b5a4f19dce78bac
Zhao Lu <zlu@zlu-imac.local>
2010-06-13 10:25:44 -0700

    added sip and skype validation for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates_format_of :sip, :with => /(\Asip:)(.*)@(.*)/, :on => :create, :allow_nil => true, :allow_blank => true
+
+  before_create :sanitize_numbers
+
+  def sanitize_numbers
+    skype.gsub!(" ", "") unless skype.nil?
+  end
 end
