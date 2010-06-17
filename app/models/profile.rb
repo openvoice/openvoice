3e39e495c9a87c33540a248a551dad4ef3e53941
Zhao Lu <zlu@myopenvoice.org>
2010-06-17 14:24:21 -0700

    require the presence of voice and messaging token as well as tropo url

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,10 @@ class Profile < ActiveRecord::Base
   belongs_to :user
 
   validates_format_of :sip, :with => /(\Asip:)(.*)@(.*)/, :on => :create, :allow_nil => true, :allow_blank => true
-
+  validates_presence_of :call_url
+  validates_presence_of :voice_token
+  validates_presence_of :messaging_token
+  
   before_create :sanitize_numbers
 
   def sanitize_numbers
