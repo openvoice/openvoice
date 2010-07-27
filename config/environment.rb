114324d5f6a1906484309a7b62837f367184c488
Zhao Lu <zlu@me.com>
2010-07-27 16:57:44 -0700

    Revert "provisioning change"
    This reverts commit 2a9f8ba315cc77d847b60ec9d45f42aea7c3c6e7.

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "aws-s3", :lib => "aws/s3"
   config.gem "tropo-webapi-ruby"
-  config.gem "tropo-provisioning"
   config.gem "authlogic"
   config.gem "will_paginate"
   
