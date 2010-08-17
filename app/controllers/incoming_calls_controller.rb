38a99352c06661ca7fe4b09071abaa7e55449cca
Zhao Lu <zlu@me.com>
2010-08-17 01:27:38 -0700

    added listen in feature, callee press 3 to listen in, press * to pick up the call, caller can leave voicemail while callee listens in

--- app/controllers/incoming_calls_controller.rb
+++ app/controllers/incoming_calls_controller.rb
@@ -5,7 +5,7 @@ class IncomingCallsController < ApplicationController
   def index
     @user = current_user
     @incoming_calls = @user.incoming_calls.reverse
-    @incoming_calls = @incoming_calls.paginate(:page => params[:page],:per_page => 15)
+    @incoming_calls = @incoming_calls.paginate(:page => params[:page], :per_page => 15)
     respond_to do |format|
       format.html
       format.xml { render :xml => @incoming_calls }
@@ -38,10 +38,10 @@ class IncomingCallsController < ApplicationController
       if @incoming_call.save
         flash[:notice] = 'CallLog was successfully created.'
         format.html { redirect_to(@incoming_call) }
-        format.xml  { render :xml => @incoming_call, :status => :created, :location => @incoming_call }
+        format.xml { render :xml => @incoming_call, :status => :created, :location => @incoming_call }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @incoming_call.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @incoming_call.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -52,14 +52,8 @@ class IncomingCallsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(user_incoming_calls_path) }
-      format.xml  { head :ok }
+      format.xml { head :ok }
     end
-  end
-
-  def jumpin
-    p "+++++++++++++++++++++++++++++++++++++++++"
-    p "inside of jumpin"
-    p "+++++++++++++++++++++++++++++++++++++++++"
   end
 
   def user_menu
@@ -68,8 +62,24 @@ class IncomingCallsController < ApplicationController
 #    return if params[:result][:state] == "DISCONNECTED"
     p "+++++++++++++++++ in user_menu"
 
-    value = params[:result][:actions][:value]
+    actions = params[:result][:actions]
+
+    value = actions[:value] ? actions[:value] : actions[:terminator]
+
     conf_id = params[:conf_id]
+
+    if value == "ring"
+      tropo = Tropo::Generator.new do
+        # TODO due to the way prophecy works, have to start the audio mixer by saying something before joining the conference,
+        # otherwise there will be no audio for either party
+        say 'foo bar'
+        conference(:name => "conference",
+                   :id => conf_id,
+                   :terminator => "*")
+      end
+      render :json => tropo.response
+    end
+
     case value
       when "connect"
         tropo = Tropo::Generator.new do
@@ -96,19 +106,18 @@ class IncomingCallsController < ApplicationController
       when "listenin"
         caller_id = CGI::escape(params[:caller_id])
         user_id = params[:user_id]
-        user_name = User.find(user_id).name
         transcription_id = user_id + "_" + Time.now.to_i.to_s
+        record_url = "#{SERVER_URL}/voicemails/create?caller_id=#{caller_id}&transcription_id=" + transcription_id + "&user_id=" + user_id
         tropo = Tropo::Generator.new do
-          say ("you have reached #{user_name}\'s voicemail.  Please speak after the beep.")
-          on(:event => 'jumpin', :next => "/incoming_calls/jumpin?user_id=#{user_id}&caller_id=#{caller_id}")
+          on(:event => 'continue', :next => "user_menu?conf_id=#{CGI.escape(conf_id)}&user_id=#{user_id}")
           start_recording(:name => "recording",
-            :format => "audio/wav",
-            :url => "#{SERVER_URL}/voicemails/create?caller_id=#{caller_id}&transcription_id=" + transcription_id + "&user_id=" + user_id
+                          :format => "audio/wav",
+                          :url => record_url
           )
           conference(:name => "conference",
                      :id => conf_id,
                      :mute => true,
-                     :terminator => "*")
+                     :terminator => "ring(*)")
         end
         render :json => tropo.response
     end
