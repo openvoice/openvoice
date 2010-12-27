126720d28e8f54df813ac333c4c37bb69692b0c5
zhao lu <zlu@me.com>
2010-12-27 03:08:17 -0800

    user can now reply text in-line.  message#index page now auto pulls new msg from server every 60 seconds.  new message will be high-lighted green on div's right border.

--- app/controllers/messagings_controller.rb
+++ app/controllers/messagings_controller.rb
@@ -7,11 +7,23 @@ class MessagingsController < ApplicationController
   def index
     @user = current_user
     @messagings = @user.messagings.reverse
-    @messagings = @messagings.paginate(:page => params[:page],:per_page => 15)
+    @new_message = Messaging.new(:text => "")
     respond_to do |format|
-      format.html
-      format.json { render :json => @messagings }
-      format.xml  { render :xml => @messagings }
+      format.html {
+        @messagings = @messagings.paginate(:page => params[:page],:per_page => 15)
+      }
+      format.json {
+        @messagings = @messagings.paginate(:page => params[:page],:per_page => 15)
+        render :json => @messagings
+      }
+      format.xml  {
+        @messagings = @messagings.paginate(:page => params[:page],:per_page => 15)
+        render :xml => @messagings
+      }
+      format.js {
+        # TODO since we have @user.messaging already, we can use sorted array  instead of another db query
+        @messagings = @user.messagings.where("updated_at > ?", Time.at(params[:after].to_i + 1))
+      }
     end
   end
 
@@ -59,6 +71,7 @@ class MessagingsController < ApplicationController
                                                               :user_id => current_user.id,
                                                               :outgoing => true }))
         @messaging.to = Contact.find(params[:contact_id]).number if(params[:contact_id])
+        @messaging.to = params[:to] if(params[:to])
       else
         # incoming request, build request to tropo for outbound sms
         p "+++++++++++++++sending TropoML payload for outbound SMS"
